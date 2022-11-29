d4e0634daeca1ee18c58b91082a9ba0416face57
SHIVAM SINGH <shivam221098@gmail.com>
2022-11-29 23:57:55 +0530

    Code Changes

--- tests/test_create_token.py
+++ tests/test_create_token.py
@@ -22,7 +22,7 @@ def test_create_access_token(Authorize):
     with pytest.raises(TypeError,match=r"fresh"):
         Authorize.create_access_token(subject="test",fresh="lol")
 
-    with pytest.raises(AttributeError,match=r"dictionary update sequence element"):
+        with pytest.raises(AttributeError,match=r"'str' object has no attribute 'get'"):
         Authorize.create_access_token(subject=1,headers="test")
 
 def test_create_refresh_token(Authorize):
@@ -32,7 +32,7 @@ def test_create_refresh_token(Authorize):
     with pytest.raises(TypeError,match=r"subject"):
         Authorize.create_refresh_token(subject=0.123)
 
-    with pytest.raises(ValueError,match=r"dictionary update sequence element"):
+    with pytest.raises(AttributeError,match=r"'str' object has no attribute 'get'"):
         Authorize.create_refresh_token(subject=1,headers="test")
 
 def test_create_dynamic_access_token_expires(Authorize):
